8f5fa4c4f9349a7d1189a1888fa564fa352540a7
JulieStenning <42295062+JulieStenning@users.noreply.github.com>
2026-07-31 16:51:19 +0100

    Changed index prefix from ux to ix to match existing naming convention

--- migrations/20260526000001_admin_case_insensitive_uniques.down.sql
+++ migrations/20260526000001_admin_case_insensitive_uniques.down.sql
@@ -1,4 +1,4 @@
-DROP INDEX IF EXISTS ux_designers_name_ci;
-DROP INDEX IF EXISTS ux_sources_name_ci;
-DROP INDEX IF EXISTS ux_hoops_name_ci;
-DROP INDEX IF EXISTS ux_tags_description_ci;
+DROP INDEX IF EXISTS ix_designers_name_ci;
+DROP INDEX IF EXISTS ix_sources_name_ci;
+DROP INDEX IF EXISTS ix_hoops_name_ci;
+DROP INDEX IF EXISTS ix_tags_description_ci;
